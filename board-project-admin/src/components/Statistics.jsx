9e76629021c3605361035dae93e781d2223c1eb9
khcmh2502 <khcmh2502@gmail.com>
2025-05-29 14:14:12 +0900

    7일내 신규가입자 조회

--- board-project-admin/src/components/Statistics.jsx
+++ board-project-admin/src/components/Statistics.jsx
@@ -6,6 +6,20 @@ export default function Statistics() {
   const [likeCountData, setLikeCountData] = useState(null);
   const [commentCountData, setCommentCountData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [newMembers, setNewMembers] = useState(null);
+
+  // 7일내 신규회원 리스트 조회
+  const fetchNewMembers = async () => {
+    try {
+      const response = await axiosApi.get("/admin/newMember");
+
+      if (response.status === 200) {
+        setNewMembers(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   // 최대 조회 수 게시글 조회
   const getMaxReadCount = async () => {
@@ -52,6 +66,7 @@ export default function Statistics() {
   // 컴포넌트가 처음 마운트 될 때 딱 1번만 실행
   // -> Statistics 컴포넌트가 화면에 마운트 될 때 서버로 세가지 데이터 요청, 응답받아야함.
   useEffect(() => {
+    fetchNewMembers();
     getMaxReadCount();
     getMaxLikeCount();
     getMaxCommentCount();
@@ -63,17 +78,21 @@ export default function Statistics() {
     if (
       readCountData != null &&
       likeCountData != null &&
-      commentCountData != null
+      commentCountData != null &&
+      newMembers != null
     ) {
       setIsLoading(false);
     }
-  }, [readCountData, likeCountData, commentCountData]);
+  }, [readCountData, likeCountData, commentCountData, newMembers]);
 
   if (isLoading) {
     return <h1>Loading...</h1>;
   } else {
     return (
       <div>
+        {/* 7일 이내 가입한 신규 회원 조회 */}
+        <NewMembers data={newMembers} />
+
         <section className="statistics-section">
           <h2>가장 조회수 많은 게시글</h2>
           <p>게시판 종류 : {readCountData.boardName}</p>
@@ -110,3 +129,34 @@ export default function Statistics() {
     );
   }
 }
+
+// 신규 회원 조회
+const NewMembers = ({ data }) => {
+  return (
+    <div className="new-members">
+      <h2>신규 가입 회원 ({data.length}명)</h2>
+      <h3>[ 7일 이내 가입 회원 ]</h3>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>회원번호</th>
+            <th>이메일</th>
+            <th>닉네임</th>
+            <th>가입일</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {data.map((member) => (
+            <tr key={member.memberNo}>
+              <td>{member.memberNo}</td>
+              <td>{member.memberEmail}</td>
+              <td>{member.memberNickname}</td>
+              <td>{member.enrollDate}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
