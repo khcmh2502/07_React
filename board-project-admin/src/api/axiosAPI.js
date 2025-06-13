0899340156799bfb7d782cd668133ef131f6eb2b
khcmh2502 <khcmh2502@gmail.com>
2025-06-13 10:03:38 +0900

    쿠키포함 설정 추가

--- board-project-admin/src/api/axiosAPI.js
+++ board-project-admin/src/api/axiosAPI.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const axiosApi = axios.create({
   baseURL: "https://cmh-boardproject.store",
+  withCredentials: true,
   // headers : {"Content-Type" : "application/json"}
   // withCredentials : true // 쿠키 포함 설정
   // 서버에서도 클라이언트가 보낸 쿠키를 받아줄 준비 해야함!
